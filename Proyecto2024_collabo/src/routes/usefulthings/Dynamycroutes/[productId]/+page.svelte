42ac44489f5329b70a45a6dcb95919bc0edcdce7
ATREUSR <romanljsa@gmail.com>
2024-05-15 21:56:31 -0300

    feat: testing sveltekit

---
+++ Proyecto2024_collabo/src/routes/usefulthings/Dynamycroutes/[productId]/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import { page } from '$app/stores';
+    const productId = $page.params.productId;
+</script>
+
+
+<h1>yeah dynamic routes nr: {productId}</h1>
---
+++ Proyecto2024_collabo/src/routes/usefulthings/Dynamycroutes/[productId]/reviews/[reviewsId]/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import { page } from '$app/stores';
+    const { productId, reviewsId } = $page.params;
+</script>
+
+<h1>review {reviewsId} for product {productId}</h1>
