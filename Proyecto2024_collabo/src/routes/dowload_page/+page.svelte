2785c4416ea86c189dbb6dde1523081ae3bbc6ff
ATREUSR <romanljsa@gmail.com>
2024-05-27 07:00:59 -0300

    feat: strating dowload page

---
+++ Proyecto2024_collabo/src/routes/dowload_page/+page.svelte
@@ -0,0 +1,16 @@
+<div class="upload-container">
+    <h1>Post</h1>
+    <img src="" alt="">
+    <h2>NLE CHOPPA TYPE LOOP | TRAP CHORDS</h2> <!-- esta linea usa valores planos pero que van a ser variables -->
+    <img src="" alt="">
+    <p>@artisit you follow</p> <!-- esta linea tambien -->
+    <p>upload 30 mins ago</p> <!-- esta linea tambien -->
+    <audio controls>
+            <source src="horse.ogg" type="audio/ogg">
+            <source src="horse.mp3" type="audio/mpeg">
+            Your browser does not support the audio element.
+    </audio><!-- aca lo del audio que tengo que ver como se hace -->
+    <button>collab</button>
+
+
+</div>
