5f397a70bb56bd8eab91fea7b252e2aeac9d451e
ATREUSR <romanljsa@gmail.com>
2024-06-25 23:23:59 -0300

    feat: starting discover page

---
+++ Proyecto2024_collabo/src/routes/discover_page/+page.svelte
@@ -0,0 +1,13 @@
+<div class="discover-container">
+    <h1>Discover</h1>
+    <div class="gradien-line"></div>
+    <div class="filter-loops-container">
+        <div class="search-container">
+            <input type="text" placeholder="Search Stuff" id="search_bar">
+        </div>
+        <div class="loops-container">
+
+        </div>
+        <div class="artist-container"></div>
+    </div>
+</div>
