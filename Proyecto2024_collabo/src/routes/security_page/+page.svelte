2881cc30c217415a04e6bc182af9d7f244c0c37e
ATREUSR <romanljsa@gmail.com>
2024-08-14 23:35:10 -0300

    Feat: yourLops and Security pages done(without backend)

--- Proyecto2024_collabo/src/routes/security_page/+page.svelte
+++ Proyecto2024_collabo/src/routes/security_page/+page.svelte
@@ -1,24 +1,162 @@
+<script>
+    import imagen from '../images/brawlstars.png';
+    import pfp1 from '../images/artista.png';
+    import pfp from '../images/artista2.png';
+</script>
+
 <div class="security-container">
     <div class="title">
         <h1>Security</h1>
         <div class="gradient-line"></div>
     </div>
     <div class="loop-info">
-        <img class="loop-img" src="" alt="">
-        <h2 class="loop-name">a</h2>
-        <span class="loop-user"></span>
-        <span class="loop-uploaded-time"></span>
+        <img class="loop-img" src={imagen} alt="">
+        <div class="loop-details">
+            <h2 class="loop-name">brawl</h2>
+            <div class="artist-info">
+                <img class="artist-pfp" src={pfp1} alt="">
+                <span class="loop-user">@dante</span>
+            </div>
+            <span class="loop-uploaded-time">uploaded 2 days ago</span>
+        </div>
     </div>
     <div class="collab-record">
         <div class="record-subtitle">
-            <h2>User</h2>
+            <h2>user</h2>
             <div class="gradient-line"></div>
         </div>
         <div class="users-list">
             <div class="user">
-                <img class="user-pfp" src="" alt="">
-                <h2 class="user-name">U</h2>
+                <img class="user-pfp" src={pfp} alt="">
+                <span class="user-name">@collabouser</span>
+            </div>
+            <div class="user">
+                <img class="user-pfp" src={pfp} alt="">
+                <span class="user-name">@collabouser</span>
+            </div>
+            <div class="user">
+                <img class="user-pfp" src={pfp} alt="">
+                <span class="user-name">@collabouser</span>
+            </div>
+            <div class="user">
+                <img class="user-pfp" src={pfp} alt="">
+                <span class="user-name">@collabouser</span>
             </div>
         </div>
     </div>
 </div>
+
+<style>
+    @import url('https://fonts.cdnfonts.com/css/utendo');
+    
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Utendo', sans-serif;
+    }
+
+    .title h1 {
+        font-size: 25px;
+        margin-bottom: 10px;
+    }
+
+    .gradient-line {
+        height: 6px;
+        background: linear-gradient(to right, #B700BD, #4800B6);
+        border-radius: 5px;
+        margin-top: 10px;
+        margin-bottom: 10px;
+    }
+
+    .security-container {
+        padding: 10px;
+        margin: 0px 100px 0px 100px;
+    }
+
+    .loop-info {
+        display: flex;
+        align-items: center;
+        margin-top: 20px;
+    }
+
+    .loop-img {
+        width: 150px;
+        height: 150px;
+        margin-right: 20px;
+    }
+
+    .loop-details {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .artist-info {
+        display: flex;
+        align-items: center;
+        margin: 5px 0;
+    }
+
+    .artist-pfp {
+        width: 35px;
+        height: 35px;
+        border-radius: 50%;
+        margin-right: 10px;
+    }
+
+    .loop-name, .loop-user, .loop-uploaded-time {
+        margin: 5px 0;
+    }
+
+    .collab-record {
+        padding: 10px;
+        border: 1px solid #FFFFFF;
+        margin-top: 10px;
+        border-radius: 15px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.5); 
+    }
+
+    .users-list {
+        display: flex;
+        flex-direction: column;
+        max-height: 150px; 
+        overflow-y: auto;
+    }
+
+    .users-list::-webkit-scrollbar {
+        width: 10px; /* Ancho de la barra de desplazamiento */
+    }
+
+    .users-list::-webkit-scrollbar-track {
+        background: #E9E9E9; /* Color de fondo de la pista de desplazamiento */
+        border-radius: 10px;
+    }
+
+    .users-list::-webkit-scrollbar-thumb {
+        background: #4800B6; /* Color de la barra de desplazamiento */
+        border-radius: 10px;
+    }
+
+    .users-list::-webkit-scrollbar-thumb:hover {
+        background: #935ce0; /* Color de la barra de desplazamiento al pasar el mouse */
+        cursor: pointer;
+    }
+
+    .user {
+        display: flex;
+        align-items: center;
+        margin: 10px 0;
+    }
+
+    .user-pfp {
+        width: 35px;
+        height: 35px;
+        border-radius: 50%;
+        margin-right: 10px;
+    }
+
+    .user-name {
+        font-size: 16px;
+        font-weight: 500;
+    }
+</style>
--- Proyecto2024_collabo/src/routes/yourLoops_page/+page.svelte
+++ Proyecto2024_collabo/src/routes/yourLoops_page/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+    import imagen from '../images/brawlstars.png';
+</script>
+
 <div class="yourloops-container">
     <div class="title">
         <h1>Uploads</h1>
@@ -5,24 +9,160 @@
     </div>
     <div class="loops-container">
         <div class="categories-container">
-            <h2>loop</h2>
-            <h2>plays</h2>
-            <h2>collabs</h2>
-            <h2>likes</h2>
-            <h2>comments</h2>
-            <h2>date</h2>
-            <div class="gradient-line"></div>
+            <h2 class="loop-categorie">loop</h2>
+            <h2 class="plays-categorie">plays</h2>
+            <h2 class="collabs-categorie">collabs</h2>
+            <h2 class="likes-categorie">likes</h2>
+            <h2 class="comments-categorie">comments</h2>
+            <h2 class="date-categorie">date</h2>
         </div>
-        <div class="loops-container">
+        <div class="gradient-line"></div>
+        <div class="loops">
             <div class="loop">
-                <img class="loop-img" src="" alt="">
-                <h2 class="loop-name">w</h2>
-                <h2 class="loop-plays">w</h2>
-                <h2 class="loop-collabs">w</h2>
-                <h2 class="loop-likes">w</h2>
-                <h2 class="loop-comments">w</h2>
-                <h2 class="loop-date">w</h2>
+                <div class="loop-info">
+                    <img class="loop-img" src={imagen} alt="">
+                    <h2 class="loop-name">Brawl</h2>
+                </div>
+                <h2 class="loop-plays">47</h2>
+                <h2 class="loop-collabs">55</h2>
+                <h2 class="loop-likes">237</h2>
+                <h2 class="loop-comments">50</h2>
+                <h2 class="loop-date">27/8</h2>
+            </div>
+            <div class="loop">
+                <div class="loop-info">
+                    <img class="loop-img" src={imagen} alt="">
+                    <h2 class="loop-name">Brawl</h2>
+                </div>
+                <h2 class="loop-plays">47</h2>
+                <h2 class="loop-collabs">55</h2>
+                <h2 class="loop-likes">237</h2>
+                <h2 class="loop-comments">50</h2>
+                <h2 class="loop-date">27/8</h2>
+            </div>
+            <div class="loop">
+                <div class="loop-info">
+                    <img class="loop-img" src={imagen} alt="">
+                    <h2 class="loop-name">Brawl</h2>
+                </div>
+                <h2 class="loop-plays">47</h2>
+                <h2 class="loop-collabs">55</h2>
+                <h2 class="loop-likes">237</h2>
+                <h2 class="loop-comments">50</h2>
+                <h2 class="loop-date">27/8</h2>
+            </div>
+            <div class="loop">
+                <div class="loop-info">
+                    <img class="loop-img" src={imagen} alt="">
+                    <h2 class="loop-name">Brawl</h2>
+                </div>
+                <h2 class="loop-plays">47</h2>
+                <h2 class="loop-collabs">55</h2>
+                <h2 class="loop-likes">237</h2>
+                <h2 class="loop-comments">50</h2>
+                <h2 class="loop-date">27/8</h2>
+            </div>
+            <div class="loop">
+                <div class="loop-info">
+                    <img class="loop-img" src={imagen} alt="">
+                    <h2 class="loop-name">Brawl</h2>
+                </div>
+                <h2 class="loop-plays">47</h2>
+                <h2 class="loop-collabs">55</h2>
+                <h2 class="loop-likes">237</h2>
+                <h2 class="loop-comments">50</h2>
+                <h2 class="loop-date">27/8</h2>
             </div>
         </div>
     </div>
 </div>
+
+<style>
+    @import url('https://fonts.cdnfonts.com/css/utendo');
+    
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Utendo', sans-serif;
+    }
+
+    .yourloops-container {
+        padding: 10px;
+        margin: 0px 100px 0px 100px;
+    }
+
+    .title h1 {
+        font-size: 25px;
+    }
+
+    .gradient-line {
+        height: 6px;
+        background: linear-gradient(to right, #B700BD, #4800B6);
+        border-radius: 5px;
+        margin: 20px 0;
+    }
+
+    .loops-container {
+        padding: 10px;
+        border: 1px solid #FFFFFF;
+        margin-top: 10px;
+        border-radius: 15px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.5); 
+    }
+
+    .categories-container {
+        display: grid;
+        grid-template-columns: repeat(6, 1fr);
+        margin-bottom: 10px;
+        margin-left: 10px;
+    }
+
+    .loops {
+        display: flex;
+        flex-direction: column;
+        max-height: 250px; 
+        overflow-y: auto;
+    }
+
+    .loops::-webkit-scrollbar {
+        width: 10px; /* Ancho de la barra de desplazamiento */
+    }
+
+    .loops::-webkit-scrollbar-track {
+        background: #E9E9E9; /* Color de fondo de la pista de desplazamiento */
+        border-radius: 10px;
+    }
+
+    .loops::-webkit-scrollbar-thumb {
+        background: #4800B6; /* Color de la barra de desplazamiento */
+        border-radius: 10px;
+    }
+
+    .loops::-webkit-scrollbar-thumb:hover {
+        background: #935ce0; /* Color de la barra de desplazamiento al pasar el mouse */
+        cursor: pointer;
+    }
+
+    .loop {
+        display: grid;
+        grid-template-columns: repeat(6, 1fr);
+        align-items: center;
+        padding: 10px 0;
+    }
+
+    .loop-info {
+        display: flex;
+        align-items: center;
+    }
+
+    .loop-img {
+        width: 50px;
+        height: 50px;
+        margin-right: 10px;
+    }
+
+    .loop h2 {
+        margin: 0 10px;
+    }
+</style>
