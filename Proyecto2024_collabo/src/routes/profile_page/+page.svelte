21364215486c48379278cee7f2dc61a3163d495d
ATREUSR <romanljsa@gmail.com>
2024-12-02 21:25:33 -0300

    seguimso rpobandooo

--- Proyecto2024_collabo/src/routes/profile_page/+page.svelte
+++ Proyecto2024_collabo/src/routes/profile_page/+page.svelte
@@ -125,7 +125,7 @@
                 title: loop.Title,
                 audioFile: loop.id, 
                 name: loop.Name,
-                artistName: loop.user,
+                artistName: loop.userId,
             }));
 
             userName = userId.name;
